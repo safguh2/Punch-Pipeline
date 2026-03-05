0e406fc6ac7ad39c122000e871d7897e9ce2c133
unknown <sirotaerik0110@gmail.com>
2026-03-05 17:02:36 +0200

    extracted the logic to other files

---
+++ pipeline.py
@@ -0,0 +1,24 @@
+import asyncio
+import schema
+import ast
+import configluz
+from sources import start_stream
+
+async def consume_data(queue, verifier, api):
+    while True:
+        data = await queue.get()
+        obj = ast.literal_eval(data.decode('utf-8'))
+        if(verifier.verify(obj)):
+            api.send(obj)
+        queue.task_done()
+
+
+async def main():
+    api, kafka_address = configluz.loadConfig()
+    verifier = schema.SchemaValidator(api)
+    queue: asyncio.Queue = start_stream(kafka_address)
+    await consume_data(queue, verifier, api)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- producer.py
+++ producer.py
@@ -1,18 +1,26 @@
+import json
+import random
+import time
 from kafka import KafkaProducer
 
-print("connecting...")
-
+# Create producer with JSON serializer
 producer = KafkaProducer(
-    bootstrap_servers="localhost:9092"
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')  # <-- serialize to JSON bytes
 )
+data = []
+for x in range(0,20):
+    data.append({"label": "test", "fields": {"test": x}})
 
-print("sending message")
+print(data)
+# Send to topic
+for item in data:
+    producer.send('test-topic', value=item)
+    print(f"test number {item['fields']['test']} has been sent")
+    time.sleep(random.randint(0,3))
 
-future = producer.send("test-topic", b"hello-from-python")
 
-result = future.get(timeout=10)
-
-print("message sent", result)
-
+# Make sure it actually gets sent
 producer.flush()
 
+print("JSON message sent!")
