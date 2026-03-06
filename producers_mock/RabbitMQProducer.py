664964091d6ce3fa8589869b2b30fde01c1ecb9f
unknown <sirotaerik0110@gmail.com>
2026-03-06 11:35:24 +0200

    added some mock data into the producer

--- producers_mock/RabbitMQProducer.py
+++ producers_mock/RabbitMQProducer.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 import pika
 
 connection = pika.BlockingConnection(
@@ -8,11 +11,24 @@ channel = connection.channel()
 
 channel.queue_declare(queue="test")
 
-channel.basic_publish(
-    exchange="",
-    routing_key="test",
-    body="hello"
-)
+data = []
+for x in range(0,20):
+    data.append({"label": "Department", "fields": {"code": str(x), "department_id": f'00{str(x)}',
+                                             "head_name": "gluz", "name": "gluz_research",
+                                             "org_id": "6767"
+                                             }})
+
+print(data)
+# Send to topic
+for item in data:
+    channel.basic_publish(
+        exchange="",
+        routing_key="test",
+        body=str(item).encode()
+    )
+    print(f"test number {item['fields']['code']} has been sent")
+    print(item)
+    time.sleep(random.randint(0,3))
 
 print("sent")
 
