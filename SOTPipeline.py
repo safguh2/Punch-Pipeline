8c76228537dd66ace7a56d01a36c40bb49429586
unknown <sirotaerik0110@gmail.com>
2026-03-15 10:44:08 +0200

    fixed the timer for backuping

--- SOTPipeline.py
+++ SOTPipeline.py
@@ -7,9 +7,8 @@ from Communication.Communication import Communication
 async def backup_pipeline(communication: Communication, interval: int = 21600):
     while True:
         try:
-            # await asyncio.sleep(interval)
+            await asyncio.sleep(interval)
             communication.update_sot()
-            await asyncio.sleep(30)
 
         except Exception as e:
             print(e)
